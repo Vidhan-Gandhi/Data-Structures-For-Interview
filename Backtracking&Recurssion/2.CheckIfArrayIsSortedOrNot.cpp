d6fd600b064a02c6bd08155ab0e4ecba87de6cb4
Vidhan Gandhi <vgbtech2327@gmail.com>
2026-07-27 10:49:20 +0530

    Check if array is sorted or not

---
+++ Backtracking&Recurssion/2.CheckIfArrayIsSortedOrNot.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSorted(vector<int>& arr, int n)
+{
+    if(n==1 || n==0)
+    {
+        return true;
+    }
+    if(arr[n-1]>=arr[n-2])
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+    isSorted(arr, n-1);
+
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 6, 5};
+    int n = arr.size();
+    if(isSorted(arr, n))
+    {
+        cout << "Array is sorted";
+    }
+    else
+    {
+        cout << "Array is not sorted";
+    }
+}
